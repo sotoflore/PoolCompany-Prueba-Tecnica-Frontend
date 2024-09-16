4c7ba32b3f69524e7d18cb36eb1c02720e61753a
francisco soto  flores <soto_flores98@hotmail.com>
2024-09-16 14:38:56 -0500

    close navbar

--- js/main.js
+++ js/main.js
@@ -101,3 +101,12 @@ document.addEventListener('DOMContentLoaded', function () {
         offset: 50
     });
 });
+
+function closeNavbar() {
+    const navbar = document.querySelector('.navbar-collapse');
+    if (navbar.classList.contains('show')) {
+        navbar.classList.remove('show');
+    }
+}
+document.addEventListener('click', closeNavbar);
+
